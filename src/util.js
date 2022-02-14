2f16667a61fd8c6b3996dbb0fb638967b005f2e5
yuhmanashi <yzxu03@gmail.com>
2022-02-14 14:20:20 -0800

    trying to fix image loading

--- src/util.js
+++ src/util.js
@@ -15,7 +15,7 @@ const Util = {
         function preloadImage(src, images, callback){
             const img = new Image();
             img.onload = callback;
-            img.src = `../assets/images/${src}.png`;
+            img.src = `./assets/images/${src}.png`;
             images.push(img);
         };
     },
